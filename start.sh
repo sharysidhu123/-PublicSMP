6ea4a5b086196307989be60d5fae9c259e1cbf94
sharry111 <sharrysidhu130@gmail.com>
2025-05-11 07:36:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# Start FTP server in background
+# Start vsftpd in the background
 service vsftpd start
 
-# Start ttyd on port 8080
-ttyd -p 8080 bash
+# Run ttyd with login shell (to allow interaction)
+ttyd -p 8080 --credential ftpuser:password login
